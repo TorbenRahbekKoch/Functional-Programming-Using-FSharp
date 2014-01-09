ed8bb1544c71024d1401d4edfde1af1d853d2109
Torben Rahbek Koch <torben@rahbekkoch.dk>
2014-01-09 21:40:11 +0100

    Exercise 3.1.

--- Chapter_03.fs
+++ Chapter_03.fs
@@ -1,3 +1,53 @@
 ﻿module Chapter_03
-open Chapter_01
+
+// 3.1 time test
+
+// Define an enum and a record type to hold a time.
+type Meridiem = AM | PM
+
+type Time = {
+    AmPm : Meridiem
+    Hour : int;
+    Minute : int;
+}
+
+// Due to the built-in value comparison of records with comparable members
+// the comparison of Time records doesn't need any specific implementation, since
+// we have ordered the members deliberately to reflect that AmPm is more important than Hour, etc.
+// If the record had been defined as below, it would not work:
+
+type Time2 = {
+    Minute2 : int
+    Hour2 : int
+    AmPm2 : Meridiem
+}
+
+// Try running this in Interactive (together with Time2 and Meridiem definition above) - it results
+// in the value true, which is obviously wrong:
+let result2 = { AmPm2 = AM; Hour2=11; Minute2=10 } < { AmPm2 = AM; Hour2=10; Minute2=11 }
+
+// The triple definition, on the other hand, does need a specific function to compare, when it is defined
+// as in the book with (hour, minute, meridiem) - that we are using a string for meridiem does not matter
+// since "AM" does sort before "PM".
+let lessThan (time1: int*int*string) (time2: int*int*string) =
+    let (hour1, minute1, amPm1) = time1
+    let (hour2, minute2, amPm2) = time2
+    if (amPm1 < amPm2) then
+        true
+    elif (amPm1 = amPm2 && hour1 < hour2) then
+        true
+    elif (amPm1 = amPm2 && hour1 = hour2 && minute1 < minute2) then
+        true
+    else
+        false
+
+let (&<) time1 time2 = lessThan time1 time2
+
+// If we were using (meridiem, hour, minute) instead, it would work out of the box:
+let result3a = ("AM", 11, 10) < ("PM", 11, 10) // true
+let result3b = ("AM", 11, 10) < ("AM", 11, 11) // true
+let result3c = ("AM", 11, 10) < ("AM", 11, 9)  // false
+    
+// We have now learned that F# uses the definition order of the record/tuple-members to compare by.
+// Very nice, tidy and logical! :)
 
--- Chapter_03_Tests.fs
+++ Chapter_03_Tests.fs
@@ -1,2 +1,48 @@
 ﻿module Chapter_03_Tests
+open System
+open NUnit.Framework
+open FsUnit
+open Chapter_03
+
+[<TestFixture>]
+type ``Chapter_03_Tests``() = 
+
+    [<Test>] 
+    member x.``3.1 triple - no custom compare``() = 
+        (10, 10, "AM") < (10, 11, "AM") |> should equal true
+        (10, 10, "PM") < (10, 11, "AM") |> should equal true // counter-intuitive ;)
+
+        (10, 10, "AM") < (10, 10, "PM") |> should equal true
+        (10, 10, "PM") < (10, 11, "PM") |> should equal true
+
+        (11, 59, "AM") < (1, 15, "PM") |> should equal false // counter-intuitive
+        (11, 00, "AM") < (1, 15, "AM") |> should equal false
+
+        (11, 59, "AM") < (11, 59, "AM") |> should equal false
+
+    [<Test>] 
+    member x.``3.1 triple - custom compare``() = 
+        (10, 10, "AM") &< (10, 11, "AM") |> should equal true
+        (10, 10, "PM") &< (10, 11, "AM") |> should equal false
+
+        (10, 10, "AM") &< (10, 10, "PM") |> should equal true
+        (10, 10, "PM") &< (10, 11, "PM") |> should equal true
+
+        (11, 59, "AM") &< (1, 15, "PM") |> should equal true
+        (11, 00, "AM") &< (1, 15, "AM") |> should equal false
+
+        (11, 59, "AM") &< (11, 59, "AM") |> should equal false
+
+    [<Test>] 
+    member x.``3.1 record``() = 
+        { AmPm = AM; Hour=10; Minute=10 } < { AmPm = AM; Hour=10; Minute=11 } |> should equal true
+        { AmPm = PM; Hour=10; Minute=10 } < { AmPm = AM; Hour=10; Minute=11 } |> should equal false
+
+        { AmPm = AM; Hour=10; Minute=10 } < { AmPm = PM; Hour=10; Minute=10 } |> should equal true
+        { AmPm = PM; Hour=10; Minute=10 } < { AmPm = PM; Hour=10; Minute=11 } |> should equal true
+
+        { AmPm = AM; Hour=11; Minute=59 } < { AmPm = PM; Hour=1; Minute=15 } |> should equal true
+        { AmPm = AM; Hour=11; Minute=00 } < { AmPm = AM; Hour=1; Minute=15 } |> should equal false
+
+        { AmPm = AM; Hour=11; Minute=59 } < { AmPm = AM; Hour=11; Minute=59 } |> should equal false
 
