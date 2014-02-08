93093535d87f1695f595f2a5f041afaaae58f6c1
Torben Rahbek Koch <torben@rahbekkoch.dk>
2014-02-08 17:09:48 +0100

    Chapter 4 exercise 12

---
+++ Chapter_04_ex_12.fs
@@ -0,0 +1,23 @@
+﻿module Chapter_04_ex_12
+#if INTERACTIVE
+#r "packages/Unquote.2.2.2/lib/net40/unquote.dll"
+#r "packages/NUnit.2.6.2/lib/nunit.framework.dll"
+#endif    
+open System
+open NUnit.Framework
+open Swensen.Unquote
+
+// 4.12 sum
+let sum (predicate, list)=
+    let rec sum' (predicate: int -> bool) (list: int list) (currentSum:int) =
+        match list with
+        | [] -> currentSum
+        | x::xs -> sum' predicate xs currentSum + (if predicate x then x else 0)
+    sum' predicate list 0
+
+[<TestFixture>]
+type ``Chapter_04_ex_12_Tests``() = 
+    [<Test>]
+    member x.``4.12 predicate sum``() = 
+        test <@ sum ((fun x -> x > 0), [-1;0;1]) = 1 @> 
+        test <@ sum ((fun x -> x < 0), [-1;0;1]) = -1 @> 
