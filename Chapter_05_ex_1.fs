94ac9ebe69e00995c67e5eb8144714c2bc67b313
Torben Rahbek Koch <torben@rahbekkoch.dk>
2014-03-06 21:30:16 +0100

    Chapter 5 - exercises 1 and 3

---
+++ Chapter_05_ex_1.fs
@@ -0,0 +1,25 @@
+﻿module Chapter_05_ex_1
+#if INTERACTIVE
+#r "packages/Unquote.2.2.2/lib/net40/unquote.dll"
+#r "packages/NUnit.2.6.2/lib/nunit.framework.dll"
+#endif    
+open System
+open NUnit.Framework
+open Swensen.Unquote
+
+// 5.1
+// List.filter: ('a -> bool) -> 'a list -> 'a list
+let foldedFilter predicate items =
+    List.foldBack (fun item state ->
+                    match predicate item with
+                    | false -> state 
+                    | true -> item::state
+                  )
+                  items
+                  []
+
+[<TestFixture>]
+type ``Chapter_05_ex 1_Tests``() = 
+    [<Test>]
+    member x.``5.1 foldedFilter``() = 
+        test <@ (foldedFilter (fun x -> x > 0) [-2;-1;0;1;2]) = [1;2] @>
---
+++ Chapter_05_ex_3.fs
@@ -0,0 +1,28 @@
+﻿module Chapter_05_ex_2
+#if INTERACTIVE
+#r "packages/Unquote.2.2.2/lib/net40/unquote.dll"
+#r "packages/NUnit.2.6.2/lib/nunit.framework.dll"
+#endif    
+open System
+open NUnit.Framework
+open Swensen.Unquote
+
+// 5.3 conditional sum using foldBack
+let sum predicate items =
+    List.foldBack (fun item currentSum ->
+                    match predicate item with
+                    | false -> currentSum 
+                    | true -> item + currentSum
+                  )
+                  items
+                  0
+
+test <@ (sum (fun x -> x > 0) [-1;0;1]) = 1 @> 
+test <@ (sum (fun x -> x < 0) [-1;0;1]) = -1 @> 
+
+[<TestFixture>]
+type ``Chapter_05_ex 3_Tests``() = 
+    [<Test>]
+    member x.``5.3 conditional sum using foldBack``() = 
+        test <@ (sum (fun x -> x > 0) [-1;0;1]) = 1 @> 
+        test <@ (sum (fun x -> x < 0) [-1;0;1]) = -1 @> 
